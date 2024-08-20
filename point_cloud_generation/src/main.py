Add termcolor and fixes in window resize. Implemented plate and back marker extrinsic computation

point_cloud_generation/src/main.py
@@ -5,12 +5,21 @@ sys.path.append(ROOT_DIR)
 import argparse, json
 import numpy as np
 import cv2
+from termcolor import colored
 from utils.general_utils import *
+from laser import *
+from utils.scanner_utils import *
+from back_marker import *
+from plate_marker import *
 
 
 def main(args: argparse.Namespace):
     print("***Point cloud generation script***")
-    window_size = args.window_size
+    debug = args.debug
+    print(colored("Debug mode is on", "yellow") if debug else "")
+    plate_marker_info = args.plate_marker_info
+    back_marker_size = args.back_marker_size
+    window_scaling_factor = args.window_scaling_factor
     camera_params_path = os.path.join(args.camera_params_dir, args.camera_params_name)
     video_path = os.path.join(args.video_dir, args.video_name)
     output_path = os.path.join(args.output_dir, args.output_name)
@@ -18,10 +27,10 @@ def main(args: argparse.Namespace):
     cap = cv2.VideoCapture(video_path)
     # Sanity checks
     if not cap.isOpened():
-        print("Error opening video file.")
+        print(colored("Error opening video file.", "red"))
         return
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     assert height > width, "Video frame is not in portrait mode"
     camera_matrix = None
     dist_coeffs = None
@@ -30,36 +39,65 @@ def main(args: argparse.Namespace):
         camera_matrix = np.array(camera_params["camera_matrix"])
         dist_coeffs = np.array(camera_params["distortion_coefficients"])
         error = camera_params["total_error"]
-        print(f"Camera parameters loaded successfully. Calibration error: {error:.4f}")
+        print(
+            colored(
+                f"Camera parameters loaded successfully",
+                "green",
+            )
+        )
+        print(colored(f"Calibration error: {error:.4f}", "dark_grey"))
 
     while True:
-        ret, frame = cap.read()
+        ret, original_frame = cap.read()
         if not ret:
             break
-        frame = get_undistorted_frame(
-            frame=frame, camera_matrix=camera_matrix, dist_coeffs=dist_coeffs
+        original_frame = get_undistorted_frame(
+            frame=original_frame, camera_matrix=camera_matrix, dist_coeffs=dist_coeffs
+        )
+        palette_frame = original_frame.copy()
+
+        gray_frame = find_black_objects(original_frame)
+        gray_contours, _ = cv2.findContours(
+            gray_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
+        )
+        rectangle = fit_marker_rectangle(gray_contours)
+        compute_back_marker_extrinsic(
+            rectangle,
+            camera_matrix,
+            dist_coeffs,
+            back_marker_size,
+            palette_frame,
         )
 
-        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # Red can have two distinct hue ranges in the HSV space (0-10 and 160-180)
-        # Thresholds here are fixed to take into account some color variations
-        mask1 = cv2.inRange(frame_hsv, np.array((0, 70, 230)), np.array((15, 255, 255)))
-        mask2 = cv2.inRange(
-            frame_hsv, np.array((155, 70, 230)), np.array((180, 255, 255))
+        dot_centers = find_plate_marker_cand_dot_centers(gray_contours, width, height)
+        ellipse = fit_marker_ellipse(dot_centers)
+        compute_plate_marker_extrinsic(
+            ellipse,
+            dot_centers,
+            camera_matrix,
+            dist_coeffs,
+            plate_marker_info,
+            original_frame,
+            palette_frame,
         )
-        red_mask = cv2.bitwise_or(mask1, mask2)
-        # Find contours in the mask
-        contours, _ = cv2.findContours(
-            red_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
-        cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
 
-        resized_frame = get_resized_frame(
-            frame=frame, window_size=window_size, width=width, height=height
+        original_frame_resized = get_resized_frame(
+            original_frame,
+            width=width,
+            height=height,
+            scaling_factor=window_scaling_factor,
         )
-        cv2.imshow("Frame", resized_frame)
-        if cv2.waitKey(100) & 0xFF == ord("q"):
-            print("User interrupted the process")
+        palette_frame_resized = get_resized_frame(
+            palette_frame,
+            width=width,
+            height=height,
+            scaling_factor=window_scaling_factor,
+        )
+
+        cv2.imshow("Original frame", original_frame_resized)
+        cv2.imshow("Palette frame", palette_frame_resized)
+        if cv2.waitKey(0) & 0xFF == ord("q"):
+            print(colored("User interrupted the process", "red"))
             cap.release()
             cv2.destroyAllWindows()
             return
@@ -79,11 +117,10 @@ if __name__ == "__main__":
         help="Enable debug mode (print additional information)",
     )
     parser.add_argument(
-        "--window_size",
-        type=int,
-        nargs=2,
-        default=(500, 700),
-        help="Size of the window to display the video (width, height)",
+        "--window_scaling_factor",
+        type=float,
+        default=0.4,
+        help="Scaling factor for the window size",
     )
     parser.add_argument(
         "--video_dir",
@@ -94,7 +131,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--video_name",
         type=str,
-        default="ball.mov",
+        default="cube.mov",
         help="Name of the video file",
     )
     parser.add_argument(
@@ -122,22 +159,40 @@ if __name__ == "__main__":
         help="Name of the camera parameters file",
     )
     parser.add_argument(
-        "--obj_marker_info",
+        "--plate_marker_info",
         type=str,
-        nargs=3,
-        default=("YWMBMMCCCYWBMYWBYWBC", 20, 5),
-        help="Information about the object marker (seq_string, seq_len, seq_vocabulary_len, min_pattern_len)",
+        nargs=5,
+        default=("YWMBMMCCCYWBMYWBYWBC", 20, 5, 4, 7.5),
+        help="Information about the object marker (seq_string, seq_len, seq_vocabulary_len, min_pattern_len, marker_radius_cm)",
+    )
+    parser.add_argument(
+        "--back_marker_size",
+        type=float,
+        nargs=2,
+        default=(13.0, 23.0),
+        help="Size of the back marker (width_cm, height_cm)",
     )
 
     args = parser.parse_args()
-    args.window_size = tuple(args.window_size)
-    args.obj_marker_info = tuple(args.obj_marker_info)
+    args.plate_marker_info = tuple(args.plate_marker_info)
+    args.plate_marker_info = (
+        str(args.plate_marker_info[0]),
+        int(args.plate_marker_info[1]),
+        int(args.plate_marker_info[2]),
+        int(args.plate_marker_info[3]),
+        float(args.plate_marker_info[4]),
+    )
+    args.back_marker_size = tuple(args.back_marker_size)
 
     assert (
-        len(args.obj_marker_info[0]) == args.obj_marker_info[1]
+        len(args.plate_marker_info[0]) == args.plate_marker_info[1]
     ), "Invalid sequence length"
     assert (
-        len(set(args.obj_marker_info[0])) == args.obj_marker_info[2]
+        len(set(args.plate_marker_info[0])) == args.plate_marker_info[2]
     ), "Invalid sequence vocabulary length"
+    assert args.plate_marker_info[3] > 0, "Invalid plate marker radius"
+    assert (
+        args.back_marker_size[0] > 0 and args.back_marker_size[1] > 0
+    ), "Invalid back marker size"
 
     main(args)

utils/general_utils.py
@@ -3,16 +3,10 @@ import numpy as np
 import cv2
 
 
-def get_marker_seq_start(seq_string: str, pattern: str, min_pattern_len: int) -> int:
-    assert len(pattern) >= min_pattern_len, "Invalid pattern length"
-    return seq_string.index(pattern)
-
-
 def get_resized_frame(
-    frame: np.ndarray, window_size: Tuple[int, int], width: float, height: float
+    frame: np.ndarray, width: int, height: int, scaling_factor: float
 ):
     # Resize the frame for display it
-    scaling_factor = min(window_size[0] / width, window_size[1] / height)
     new_width = int(width * scaling_factor)
     new_height = int(height * scaling_factor)
     resized_frame = cv2.resize(frame, (new_width, new_height))
@@ -26,9 +20,9 @@ def get_undistorted_frame(
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
         camera_matrix, dist_coeffs, (w, h), 1, (w, h)
     )
-    # undistort
+    # Undistort
     dst = cv2.undistort(frame, camera_matrix, dist_coeffs, None, newcameramtx)
-    # crop the image
+    # Crop the image
     x, y, w, h = roi
     dst = dst[y : y + h, x : x + w]
     return dst